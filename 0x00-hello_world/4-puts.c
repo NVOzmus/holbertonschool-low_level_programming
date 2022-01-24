cf5667cdf15485e185d5681a4f5e655a748b2367
NV Ozmus <NVOzmus@outlook.com>
2022-01-24 09:44:49 -0800

    I hate this

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	/*This is a comment because Betty makes me*/puts("\"Programming is like building a multilingual puzzle"
+	puts("\"Programming is like building a multilingual puzzle"
 );
 return (0);
 }
