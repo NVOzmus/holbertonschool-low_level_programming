b0dad9b146c6632108e239604f63931944d8d8ed
NV Ozmus <NVOzmus@outlook.com>
2022-01-24 10:54:26 -0800

    corrected comment typo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - enters the program
  *
  * Return: returns the value 0 and closes the program
- * */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
