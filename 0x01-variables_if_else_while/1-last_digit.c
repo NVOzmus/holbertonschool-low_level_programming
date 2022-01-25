2cd4bd2d233cedca522a17b5c1b2a52f5153fcab
NV Ozmus <NVOzmus@outlook.com>
2022-01-25 11:35:10 -0800

    fixed typo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,18 +15,18 @@ int main(void)
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 		LastDigit = n % 10;
-		printf("the last digit of %d is", n);
+		printf("the last digit of %d is ", n);
 if (LastDigit > 5)
 {
-	printf("and is greater than 5\n");
+	printf("%d and is greater than 5\n", LastDigit);
 }
 else if (LastDigit == 0)
 {
-	printf("and is 0\n");
+	printf("%d and is 0\n", LastDigit);
 }
 else
 {
-	printf("is less than 6 and not 0\n");
+	printf("%d and is less than 6 and not 0\n", LastDigit);
 }
 		return (0);
 }
