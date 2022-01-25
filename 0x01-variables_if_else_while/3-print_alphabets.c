e63eeaef824a926ef4ad6cfbdc026240d2618417
NV Ozmus <NVOzmus@outlook.com>
2022-01-25 12:12:45 -0800

    lol typo

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,15 @@ int main(void)
 	char lowercase;
 	char uppercase;
 
-	for (lowercase = 'a'; lower <= 'z'; lowercase++)
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 	{
 		putchar(lowercase);
 	}
 	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 	{
 		putchar(uppercase);
+	}
+	putchar('\n');
 			return (0);
+	
 }
