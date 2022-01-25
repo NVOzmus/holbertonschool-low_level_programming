fa39267731253f0a26f31fb22812aa3352ddf99e
NV Ozmus <NVOzmus@outlook.com>
2022-01-25 12:09:08 -0800

    updated with code

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - starts function
+ *
+ * Return: 0 and closes
+ */
+int main(void)
+{
+	char lowercase;
+	char uppercase;
+
+	for (lowercase = 'a'; lower <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+	}
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+	{
+		putchar(uppercase);
+			return (0);
+}
