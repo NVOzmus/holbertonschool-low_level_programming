2559ca7fa66ac73a2787702b7c6e702d4f744170
NV Ozmus <NVOzmus@outlook.com>
2022-01-25 13:28:17 -0800

    added 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - runs program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+char a;
+
+for (i = '0'; i <= '9'; i++)
+{
+	putchar(i);
+}
+for (a = 'a'; a <= 'f'; a++)
+{
+putchar(a);
+}
+putchar('\n')
+	return (0);
+}
