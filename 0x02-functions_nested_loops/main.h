d83d0986179e630350c84f2afaa5e79ace90ee54
NV Ozmus <NVOzmus@outlook.com>
2022-01-26 11:18:10 -0800

    added main just in case

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* includes */
+#include <unistd.h>
+#include <stdio.h>
+/* prototypes */
+/* _putchar prototype */
+int _putchar(char c);
+/* print alphanet prototype */
+int print_alphabet(void);
+#endif /* MAIN_H */
