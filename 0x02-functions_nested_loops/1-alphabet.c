c157f30dc1126fcb1912f4f78579ea28f5053a45
NV Ozmus <NVOzmus@outlook.com>
2022-01-26 11:26:23 -0800

    added 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - runs the program
+ * 
+ * _putchar - places character in code
+ *
+ * Return: 0 and closes the code
+ */
+
+int main(void)
+{
+	char alpha; 
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+	_putchar('\n');
+	return (0);
+}
