fb774beeeff552a8ac984996791c93b24afd56d4
NV Ozmus <NVOzmus@outlook.com>
2022-01-26 14:43:55 -0800

    deleted whitespace

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * int _abs - main function
+ *
+ * Return: 0 and closes
+ */
+int _abs(int i)
+{
+
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else if (i < 0)
+	{
+		return (-i);
+	}
+	return (0);
+}
