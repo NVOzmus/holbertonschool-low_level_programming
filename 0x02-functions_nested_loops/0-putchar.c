6a26522b501d4490d82bcb7cf0b1e5cb26a8b46f
NV Ozmus <NVOzmus@outlook.com>
2022-01-26 11:20:06 -0800

    fixed whitespace

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,6 @@
  */
 int main(void)
 {
-	
-		
 		_putchar('_');
 		_putchar('p');
 		_putchar('u');
@@ -18,8 +16,6 @@ int main(void)
 		_putchar('h');
 		_putchar('a');
 		_putchar('r');
-		_putchar('\n');
-
-	
+		_putchar('\n');	
 	return (0);
 }
