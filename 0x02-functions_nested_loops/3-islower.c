06a702fad71302a14c8ed5bbcaaf71cc1fba8bfd
NV Ozmus <NVOzmus@outlook.com>
2022-01-26 12:21:24 -0800

    added 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * islower - checks for lowercase character
+ *
+ * Return: 0, closes
+ */
+int _islower(int c)
+{
+	int case;
+
+	if (case >= 'a' and case <='z')
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
