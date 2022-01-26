8316063a2596e96fc7f156f14f592f442cc164fd
NV Ozmus <NVOzmus@outlook.com>
2022-01-26 11:53:23 -0800

    conflicting errors

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,10 +2,11 @@
 #define MAIN_H
 /* includes */
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 /* prototypes */
 /* _putchar prototype */
 int _putchar(char c);
 /* print alphabet prototype */
-int print_alphabet(void);
+void print_alphabet(void);
 #endif /* MAIN_H */
