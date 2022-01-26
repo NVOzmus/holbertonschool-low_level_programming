1dbf0e753affdf94f8fa62c91a4af96b7363c325
NV Ozmus <NVOzmus@outlook.com>
2022-01-26 14:17:10 -0800

    fixed syntax error

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,18 +2,24 @@
 /**
  * print_sign - (int n)
  * @n: integer that checks
- * 
+ *
  *Return:0
  */
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		return (1);
 		_putchar('+');
-	else if (n = 0)
+	}
+	else if (n == 0)
+	{
 		return (0);
 		_putchar('0');
+	}
 	else
-		return ('-1';
-		_putchar('-');	
+	{
+		return ('-1');
+		_putchar('-');
+	}
 }
