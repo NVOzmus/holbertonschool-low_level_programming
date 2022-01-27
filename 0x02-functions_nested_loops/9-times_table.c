5ee6791ebf8880b13bb3965eef1714dbd9bc3bd4
NV Ozmus <NVOzmus@outlook.com>
2022-01-26 18:50:33 -0800

    psuedo code round 3

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - void operator that counts
+ *
+ *
+ * Return: 0
+ */
+void times_table(void){
+
+	int l;
+	int h;
+	int p;
+
+	for (l = 0; l <= 9; l++)
+	{
+		putchar('0');
+		for h = 1; h <= 9; l++)
+		{
+			p = l * h;
+			putchar(',');
+			putchar(' ');
+			if (product <= 9)
+			{
+				putchar(' ');
+		}
+			else
+			{
+				putchar((p / 10) + '0');
+			}
+			putchar((p % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
