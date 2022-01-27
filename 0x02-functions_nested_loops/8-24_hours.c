5f3a8581bfe3231a7a0960ed030d3f0e299a1c05
NV Ozmus <NVOzmus@outlook.com>
2022-01-26 17:30:56 -0800

    how did I do all of this without nests

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,15 @@ void jack_bauer(void)
 	int m;
 
 	for (h = 0; h < 24; h++);
-	for (m = 0; m < 60; m++);
-	putchar((h / 10) + '0');
-	putchar((h % 10) + '0');
-	putchar(':');
-	putchar((m / 10) + '0');
-	putchar((m % 10) + '0');
-	putchar('\n');
+	{
+		for (m = 0; m < 60; m++);
+		{
+			putchar((h / 10) + '0');
+			putchar((h % 10) + '0');
+			putchar(':');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			putchar('\n');
+		}
+	}
 }
