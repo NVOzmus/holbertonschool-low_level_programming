58dddc1d0603b0a664b423cfddb5a8880d27b139
NV Ozmus <NVOzmus@outlook.com>
2022-01-26 17:33:15 -0800

    deleted semicolons

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 	int h;
 	int m;
 
-	for (h = 0; h < 24; h++);
+	for (h = 0; h < 24; h++)
 	{
-		for (m = 0; m < 60; m++);
+		for (m = 0; m < 60; m++)
 		{
 			putchar((h / 10) + '0');
 			putchar((h % 10) + '0');
