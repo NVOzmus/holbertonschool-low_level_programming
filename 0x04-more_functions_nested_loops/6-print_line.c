8786a83a4ad2f07cd499b45ec47f1c717fb4ab4f
NV Ozmus <NVOzmus@outlook.com>
2022-02-01 15:59:31 -0800

    created, second attempt

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - prints line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int line;
+	if (n > 0)
+	{
+		for line = 0; line < n; line++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
