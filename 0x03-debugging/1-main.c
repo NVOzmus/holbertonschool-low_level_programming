fd98c91ef897730b4fe9424767eb45de0497d8db
NV Ozmus <NVOzmus@outlook.com>
2022-02-01 09:18:48 -0800

    fixed typo

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * Return: 0 
+ * Return: 0
  */
 
 int main(void)
@@ -14,6 +14,11 @@ int main(void)
 	i = 0;
 
 	/*while (i < 10)
-	{
-		putchar(i);
-	}*/
+	* {
+	*	putchar(i);
+	* }
+	*/
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
