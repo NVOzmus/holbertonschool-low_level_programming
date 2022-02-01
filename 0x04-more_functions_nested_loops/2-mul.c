9d873a55a3137df6948986fa6f2c36d0e0427c76
NV Ozmus <NVOzmus@outlook.com>
2022-02-01 13:07:33 -0800

    fixed missing ;

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * @a: first integer checked
+ * @b: second integer checked
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int product
+	{
+		product = a * b;
+	}
+		return (0);
+}
