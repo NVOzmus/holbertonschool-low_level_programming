88d447ca88499d560286441a0b4c6f47465489e9
NV Ozmus <NVOzmus@outlook.com>
2022-02-01 17:06:36 -0800

    changed if else

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,6 +10,8 @@ void print_square(int size)
 	int x;
 	int y;
 
+	if (size > 0)
+	{
 	for (x = 0; x < size; x++)
 	{
 		for (y = 0; y < size; y++)
@@ -17,5 +19,10 @@ void print_square(int size)
 			_putchar(23);
 		}
 			_putchar('\n');
+	}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
