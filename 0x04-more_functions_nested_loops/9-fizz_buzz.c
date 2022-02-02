267bf27e367a1354d393b5585776e58e213e3c5d
NV Ozmus <NVOzmus@outlook.com>
2022-02-01 19:29:59 -0800

    fixed spacing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,20 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf(" Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf(" Buzz");
+		else if
+			(x == 1)
+			{
+			printf("%d", i);
+			}
 		else
-			printf("%d ", i);
-		printf("\n");
+		{
+		printf(" %d", i);
 	}
+	putchar('\n');
 	return (0);
 }
