be0069eed123cbc4beed9967aa206dd2ab4313b3
NV Ozmus <NVOzmus@outlook.com>
2022-02-01 16:44:09 -0800

    fixed brackets

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,17 +11,19 @@ void print_diagonal(int n)
 	int y;
 
 	if (n > 0)
-
 	{
 		for (x = 0; x < n; x++)
 		{
 			for (y = 0; y < x; y++)
+			{
 			_putchar(32);
 		}
-		{
 			_putchar(92);
 			_putchar('\n');
 		}
 	}
+	else
+	{
 	_putchar('\n');
+	}
 }
