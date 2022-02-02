1ea1f88bada5df77c197bd149a978ad1afb5ae12
NV Ozmus <NVOzmus@outlook.com>
2022-02-02 13:41:56 -0800

    initial pseudocode

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - primary function
+ * @str: primary variable
+ *
+ * Return: 0
+ */
+void puts2(char *str);
+{
+	for (int i = 0; i < strlen; str += 2);
+	{
+		putchar([str]);
+		[str / 2] = [str];
+}
