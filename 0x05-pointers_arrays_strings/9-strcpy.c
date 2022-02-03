19700db0b9d7876999d28fb15bef9734434035c4
NV Ozmus <NVOzmus@outlook.com>
2022-02-03 10:20:40 -0800

    updated typos

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,9 +9,15 @@
 char *_strcpy(char *dest, char *src)
 {
 	size_t i;
+	
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i <  && src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
-	for ( ; i < n; i++)
+	}
+	for ( ; i  i++)
+	{
 		dest[i] = '\0';
+	}
+	return (*dest);
 }
