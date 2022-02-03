cbfebba0ca5adbf4a9a4e1fd9d8d9d8d4ebd54d8
NV Ozmus <NVOzmus@outlook.com>
2022-02-03 07:24:57 -0800

    first test

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - main function
+ *@src: 
+ *
+ * Return: 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+}
