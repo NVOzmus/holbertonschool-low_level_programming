a736e81509dd70276647ba88cbb38068a5941854
NV Ozmus <NVOzmus@outlook.com>
2022-02-07 12:50:14 -0800

    added pseudocode

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * string_toupper - main function
+ *@a: variable
+ *
+ * Return: 0
+ */
+
+char *string_toupper(char *a)
+{
+	int i;
+
+	if (a[i] >= 'a' && a[i] <= 'z')
+	{
+		a[i] = a[i] - 32;
+	}
+	i++;
+	return (0)
+}
