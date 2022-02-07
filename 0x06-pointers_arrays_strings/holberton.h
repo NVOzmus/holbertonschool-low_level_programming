88317f82e289f35e13651285b497f802e912f34d
NV Ozmus <NVOzmus@outlook.com>
2022-02-07 08:27:00 -0800

    fixed up header and initial code attempt at 0

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -3,7 +3,7 @@
 /* includes */
 #include <unistd.h>
 #include <stdio.h>
-void _putchar.c
+int _putchar.c(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
