a80dc455137dc8dea44441413cd46d22b216ee34
NV Ozmus <NVOzmus@outlook.com>
2022-02-07 11:58:51 -0800

    fixed code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] && s2[i]; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '0'; i++)
 	{
-	if (s1[i] == s2[i])
-		return 0;
-	else if (s1[i] < (s2[i])
-		return -1;
-	else
-		return 1;
+		if (*s1 - *s2 == 0)
+		{
+			s1++;
+			s2++;
+		}
 	}
+	return (*s1 - *s2);
 }
-
