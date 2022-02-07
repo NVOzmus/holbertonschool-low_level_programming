47fe2587ccad7e8b85cbd467417beeff452341a3
NV Ozmus <NVOzmus@outlook.com>
2022-02-07 11:07:50 -0800

    fixed trailing whitespaces

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 
 /**
  * *_strncat - main function
- * @dest: 
+ * @dest:
  * @src
  * Return: 0
  */
@@ -11,17 +11,16 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int destlen;
 	/* measures the length of dest*/
-	for (destlen = 0; dest[destlen] !='\0'; destlen++)
+	for (destlen = 0; dest[destlen] != '\0'; destlen++)
 	{}
 
 	/* measures the length from src*/
 	for (i = 0; i < n; i++)
 	{
 		dest[destlen + i] = src[i];
-		if (src[i] = '\0')
+		if (src[i] == '\0')
 			break;
 	}
 	dest[dest_len + i] = '\0';
 	return (dest);
-		
 }
