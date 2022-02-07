a37ce5e368685ba4e8561aa6a71ada1ec60313cd
NV Ozmus <NVOzmus@outlook.com>
2022-02-07 10:37:13 -0800

    initial test

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * *_strncat - main function
+ * *_strncpy - main function
  * @dest: 
  * @src
  * Return: 0
@@ -10,17 +10,20 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int destlen;
-	/* measures the length of dest*/
-	for (destlen = 0; dest[destlen] !='\0'; destlen++)
-	{}
+
+	
 	/* measures the length from src*/
 	for (i = 0; i < n; i++)
 	{
-		dest[destlen + i] = src[i];
-		if (src[i] = '\0')
-			break;
+		dest[i] = src[i];
+		if (src[i] == '\0')
+		{
+			while (i < n)
+			{
+				dest[i] = '\0';
+				i++;
+			}
+		}
 	}
-	dest[dest_len +i] = '\0';
 	return (dest);
 }
