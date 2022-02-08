715248de7d219398c2ae43b83fd0c6ef5935ca86
NV Ozmus <NVOzmus@outlook.com>
2022-02-07 16:38:17 -0800

    misplaced ;

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *a)
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		if (a[i] >= 'a' && a[i] <= 'z';)
+		if (a[i] >= 'a' && a[i] <= 'z')
 		{
 			if (a[i - 1] == ' ' ||
 					a[i - 1] == '\t' ||
