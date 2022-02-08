be1ff729afe352066e7ff78ed0dbeb03667bd57f
NV Ozmus <NVOzmus@outlook.com>
2022-02-08 09:01:12 -0800

    added first attempt

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _memcpy - main function
+ * @dest: first variable
+ * @src: second variable
+ * @n: third variable
+ * Return: 0
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
