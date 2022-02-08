6cc34597e82fed9147cf6ef27c5985751610f0f9
NV Ozmus <NVOzmus@outlook.com>
2022-02-08 13:44:06 -0800

    fixed segmentation fault in for statement

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - main function
+ * @s: first variable
+ * @n: second variable
+ *
+ * Return: 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x;
+
+	do{
+		for (x = 0; s[x] != '\0'; x++)
+		{
+			if (accept[x] != '\0')
+			{
+				return (s);
+			}
+		}
+		s++
+	} while (*s != '\0');
+	return (0);
+}
