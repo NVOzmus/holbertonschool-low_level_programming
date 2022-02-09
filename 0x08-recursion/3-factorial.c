6f0b67cb51e0d7d2aab7278fc0c3175edf08af7d
NV Ozmus <NVOzmus@outlook.com>
2022-02-09 11:55:22 -0800

    fixed betty spacing

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,7 @@
 #include "main.h"
 int factorial(int n)
 {
-	if (n < 0 )
+	if (n < 0)
 	{
 		return (-1);
 	}
@@ -9,5 +9,5 @@ int factorial(int n)
 	{
 		return (1);
 	}
-	return (n * factorial(n -1));
+	return (n * factorial(n - 1));
 }
