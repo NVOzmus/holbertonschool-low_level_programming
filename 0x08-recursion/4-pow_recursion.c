c1bf0fcf9dbd4560d303dd346aa9037c2e64b4d5
NV Ozmus <NVOzmus@outlook.com>
2022-02-09 12:43:16 -0800

    typo in file name

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _pow_recursion - main function
+ * @x: first variable
+ * @y: second variable, to power of
+ *
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1)
+	}
+	if ( y == 0)
+	{
+		return (1)
+	}
+	if (y) 
+	{
+		return (x * _pow_recursion(x, y-1));
+	}
+	return (x);
+}
