4df2a04033f69bf26de2c06c89344e05e9af385a
NV Ozmus <NVOzmus@outlook.com>
2022-02-08 19:47:51 -0800

    fixed whitespace for bettiquette

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ void print_diagsums(int *a, int size)
 	unsigned int y = 0;
 	int sum;
 
-	for (sum =0; sum < size; sum++)
+	for (sum = 0; sum < size; sum++)
 	{
 		x = x + a[(size * sum) + sum];
 		y = y + a[(size * (sum + 1)) - (sum + 1)];
