5dd3fd775af1b63be89cb21612ea55de30e1aba2
NVOzmus <nvozmus@outlook.com>
2022-02-15 13:10:52 -0800

    added multiplier program

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - primary function
+ * @argc: arg counter
+ * @argv: arrays of the args
+ * Return: 0
+ */
+
+int main(int argc, *argv[])
+{
+	int sum = 0;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
