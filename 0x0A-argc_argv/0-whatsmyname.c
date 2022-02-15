d4d97247a9b6ca150e74a88a944fee59950c4e5c
NVOzmus <nvozmus@outlook.com>
2022-02-15 08:11:55 -0800

    added first test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * rihanna - main function
+ * Did you know I had a perfect in this class until the checker screwed me over?
+ * Return: 0, like what I'd rather take than do work today
+ */
+
+int rihanna(int argc, char *argv[])
+{
+	int i;
+	for (i=0. i < argc - 1; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	return (0);
+}
