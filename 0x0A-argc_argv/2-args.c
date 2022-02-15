2353695820feb9d2f631006481f16b8ff76ca110
NVOzmus <nvozmus@outlook.com>
2022-02-15 11:54:13 -0800

    added 2-argcs

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - main function
+ * @argc: counts number of arguments
+ * @argv: array of argument strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc, i++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
