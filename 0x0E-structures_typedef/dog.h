7166f42f595e78fda568d0225a0adcc933b7593d
NVOzmus <nvozmus@outlook.com>
2022-02-22 09:34:19 -0800

    added update to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,14 +9,16 @@
  * @age: float of age
  * @owner: char holding owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+
 
 #endif /* DOG_H */
