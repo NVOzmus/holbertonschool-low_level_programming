db63dc0fb403d5bf9977fc9826faa7f458ec96c6
NVOzmus <nvozmus@outlook.com>
2022-02-22 09:04:17 -0800

    added 1 and 1-main.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * init_dog - main function
+ * @d: struct of dog
+ * @name: pointer to the name value
+ * @age: float of the age value
+ * @owner: pointer to owner name
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
