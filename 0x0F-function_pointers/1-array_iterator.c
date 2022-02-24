d9a9aab80255d34088cdc1f85569ffc81f063190
NVOzmus <nvozmus@outlook.com>
2022-02-23 16:37:23 -0800

    fixed brackets

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
-	if(array == NULL) || (action == NULL)
-		return;
-
-	for (x = 0; x < size; x++)
+	if(array != NULL && action != NULL)
+	{
+		for (x = 0; x < size; x++)
+		{
 		action(array[x]);
+		}
+	}
 }
