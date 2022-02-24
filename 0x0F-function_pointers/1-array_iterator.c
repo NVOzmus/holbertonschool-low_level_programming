6eac5bad0bc65baabd734e6f86626d956228d042
NVOzmus <nvozmus@outlook.com>
2022-02-23 16:40:51 -0800

    bettiquette

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,16 +1,17 @@
 #include "function_pointers.h"
 /**
- *
- *
- *
- *
+ * array_iterator - main function
+ * @array: array variable
+ * @size: size
+ * @action: action
+ * Return: void
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
-	if(array != NULL && action != NULL)
+	if (array != NULL && action != NULL)
 	{
 		for (x = 0; x < size; x++)
 		{
