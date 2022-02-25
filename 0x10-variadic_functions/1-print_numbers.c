2ac827070bb52d0ec71cc9b7fedff7ba7a1b54ed
NVOzmus <nvozmus@outlook.com>
2022-02-25 15:48:44 -0800

    .

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf ("%d", va_arg(v1, int));
+		printf ("%d", va_arg(ap, int));
 		if (i < (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
