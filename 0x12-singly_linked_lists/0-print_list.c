012c6843a5bcbf8f8f720a00a16c35668acc8cb5
NVOzmus <nvozmus@outlook.com>
2022-03-07 18:34:34 -0800

    attempted correction for pedantic

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,15 @@
 size_t print_list(const list_t *h)
 {
 	size_t len = 0;
-
-	if (h == NULL)
+	
+	while (h == NULL)
 		printf("[0] (nil)");
-		return (0);
+	
 	while (h != NULL)
 	{
 		printf("[%d] %s\n", h->len, h->str);
-				h = h->next;
-				len++;
+		h = h->next;
+		len++;
 	}
 	return (len);
 }
