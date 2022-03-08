954c2669d730027ca3db6eed6e70860a08a0a000
NVOzmus <nvozmus@outlook.com>
2022-03-07 17:53:25 -0800

    fixed trailing whitespace

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
  *  print_list - function prints list
- *  @h: variable 
+ *  @h: variable
  * Return: number of nodes, else nil
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t index = 0;
-	
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
