2416be0c2a912b5887b8755d64c71eca4ec22722
NVOzmus <nvozmus@outlook.com>
2022-03-07 17:52:09 -0800

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,6 +5,22 @@
  * Return: number of nodes, else nil
  */
 
-size_t print_list(const list_t *h);
-
-
+size_t print_list(const list_t *h)
+{
+	size_t index = 0;
+	
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			index++;
+			h = h->next;
+		}
+	}
+	return (index);
+}
