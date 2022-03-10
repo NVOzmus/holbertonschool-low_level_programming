9a09d6d6c59cb73bc5881318878cad0e0f0ec8fe
NVOzmus <nvozmus@outlook.com>
2022-03-09 22:07:54 -0800

    bettiquette

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,8 +8,8 @@
  *
  *  Return: NULL
  */
- 
- listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new;
 	listint_t *temp = *head;
