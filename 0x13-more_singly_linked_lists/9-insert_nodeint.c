bd97f6e6548c00d292415988bb75c082af258c9c
NVOzmus <nvozmus@outlook.com>
2022-03-09 22:06:28 -0800

    botched formatters

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
  listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new;
-	listint_T *temp = *head;
+	listint_t *temp = *head;
 	unsigned int node;
 
 	new = malloc(sizeof(listint_t));
@@ -29,13 +29,13 @@
 	}
 	for (node = 0; node < (idx - 1); node++)
 	{
-		if (temp == NULL | temp->next == NULL)
+		if (temp == NULL || temp->next == NULL)
 		{
 			return (NULL);
 		}
 		temp = temp->next;
 	}
-	new-next = temp->next;
+	new->next = temp->next;
 	temp->next = new;
 	return (new);
 }
