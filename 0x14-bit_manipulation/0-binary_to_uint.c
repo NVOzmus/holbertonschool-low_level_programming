46522a709d3c26afd9277a3869683e45e788d1d2
NVOzmus <nvozmus@outlook.com>
2022-03-11 11:35:12 -0800

    added main.c, main.h

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: where be is the binary to convert
+ * Return: converted number, otherwise 0 
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+}
